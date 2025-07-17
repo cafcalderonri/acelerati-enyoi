6b14f5535c8cb545d1721388b1b0a20049c3092f
cafcalderonri <cafcalderonri@gmail.com>
2025-07-16 21:43:52 -0500

    microservices part 1

---
+++ clients/src/main/java/com/enyoi/clients/dto/SaveNewClientDto.java
@@ -0,0 +1,14 @@
+package com.enyoi.clients.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SaveNewClientDto {
+
+    private String name;
+    private String email;
+
+
+}
