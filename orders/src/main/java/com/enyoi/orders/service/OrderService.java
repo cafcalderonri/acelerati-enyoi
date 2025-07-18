0c58bc8a07c7df286e7282eb1e240601a61102bd
cafcalderonri <cafcalderonri@gmail.com>
2025-07-17 22:06:25 -0500

    class 2 microservices

--- orders/src/main/java/com/enyoi/orders/service/OrderService.java
+++ orders/src/main/java/com/enyoi/orders/service/OrderService.java
@@ -4,7 +4,10 @@ import com.enyoi.orders.config.EnvsFacade;
 import com.enyoi.orders.dto.ClientResponseDto;
 import com.enyoi.orders.dto.CreateNewOrderCreatingNewClientDto;
 import com.enyoi.orders.dto.GenerateNewOrderDto;
+import com.enyoi.orders.dto.ProductResponseDto;
+import com.enyoi.orders.dto.ProductShop;
 import com.enyoi.orders.model.Order;
+import com.enyoi.orders.model.ProductOrder;
 import com.enyoi.orders.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
@@ -13,32 +16,61 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
-public class OrderService { //INYECTAR LAS DEPENDENCIAS
+public class OrderService {
 
-    private final OrderRepository orderRepository; //DEPENDENCIA
-    private final RestTemplate restTemplate; //DEPENDENCIA
-    private final EnvsFacade envsFacade; //DEPENDENCIA
+    private final OrderRepository orderRepository;
+    private final RestTemplate restTemplate;
+    private final EnvsFacade envsFacade;
 
     public Order createNewOrder(GenerateNewOrderDto dto){
-        //GENERO LA URL
-        String url = envsFacade.getClientHostEnv() + envsFacade.getClientPath() + dto.getClientEmail();
+        String clientId = getClientId(dto.getClientEmail());
 
-        //LLAMO AL MICROSERVICIO DE CLIENTES
-        ResponseEntity<ClientResponseDto> responseEntityDto = restTemplate.exchange(
-                url, HttpMethod.GET, null, ClientResponseDto.class);
+        Order order = new Order();
+        order.setClientId(clientId);
 
-        //HAGO EL RESTO DE MI LOGICA
-        if(responseEntityDto.getStatusCode().is2xxSuccessful()){
-            ClientResponseDto bodyResponse = responseEntityDto.getBody();
-            Order order = new Order();
-            order.setClientId(bodyResponse.getId());
-            return orderRepository.save(order);
+        List<ProductOrder> productOrderList = new ArrayList<>();
+        for(ProductShop productShop: dto.getProductShopList()){
+            ProductOrder productOrder = new ProductOrder();
+            String productId = getProductId(productShop.getName());
+            productOrder.setOrder(order);
+            productOrder.setProductId(productId);
+            productOrderList.add(productOrder);
         }
 
-        throw new RuntimeException("ERROR");
+        order.setProductOrderList(productOrderList);
+
+        return orderRepository.save(order);
     }
+
+    private String getClientId(String clientEmail){
+        String url = envsFacade.getClientHostEnv() + envsFacade.getClientPath() + clientEmail;
+        ResponseEntity<ClientResponseDto> responseEntityDto = restTemplate.exchange(
+                url, HttpMethod.GET, null, ClientResponseDto.class);
+        return responseEntityDto.getBody().getId();
+    }
+
+    private String getProductId(String productName){
+        String url = "http://localhost:8082/api/v1/product/" + productName;
+        ResponseEntity<ProductResponseDto> responseEntity = restTemplate.exchange(
+                url, HttpMethod.GET, null, ProductResponseDto.class);
+        return responseEntity.getBody().getId();
+
+    }
+
+
+
+
+
+
+
+
+
+
 
 
     public Order createNewOrderCreatingNewClient(CreateNewOrderCreatingNewClientDto dto){
